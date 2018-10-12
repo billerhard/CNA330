8e7ee0c221733212fb6251a7328b48debaf342ce
Bill <wherhard@student.rtc.edu>
2018-10-11 21:21:18 -0700

    Initial Work, prints out the GET from request

--- JobHunter/JobHunter.py
+++ JobHunter/JobHunter.py
@@ -8,25 +8,26 @@ import urllib.request
 import os
 import time
 
+
 # Connect to database
-# You should not need to edit anything in this function
 def connect_to_sql():
     conn = mysql.connector.connect(user='root', password='',
                                   host='127.0.0.1',
                                   database='cna330')
     return conn
 
+
 # Create the table structure
 def create_tables(cursor, table):
-    ## Add your code here. Starter code below
-    cursor.execute('''CREATE TABLE IF NOT EXISTS tablename (id INT PRIMARY KEY); ''')
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} (id INT PRIMARY KEY);")
     return
 
+
 # Query the database.
-# You should not need to edit anything in this function
 def query_sql(cursor, query):
     cursor.execute(query)
     return cursor
+
 
 # Add a new job
 def add_new_job(cursor, jobdetails):
@@ -34,29 +35,38 @@ def add_new_job(cursor, jobdetails):
     query = "INSERT INTO"
     return query_sql(cursor, query)
 
+
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
     ## Add your code here
     query = "SELECT"
     return query_sql(cursor, query)
 
+
 def delete_job(cursor, jobdetails):
     ## Add your code here
     query = "UPDATE"
     return query_sql(cursor, query)
 
+
 # Grab new jobs from a website
 def fetch_new_jobs(arg_dict):
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/Sql.py
-    query = "https://jobs.github.com/positions.json?" + "location=seattle" # Add arguments here
+    query = "https://jobs.github.com/positions.json?" + "&location=seattle" # + "&description=" + description # Add arguments here
     jsonpage = 0
     try:
         contents = urllib.request.urlopen(query)
         response = contents.read()
         jsonpage = json.loads(response)
+        for job in jsonpage:
+            print(job)
+
+
     except:
+        print("whoops")
         pass
     return jsonpage
+
 
 # Load a text-based configuration file
 def load_config_file(filename):
@@ -77,6 +87,7 @@ def load_config_file(filename):
     ## Add in information for argument dictionary
     return argument_dictionary
 
+
 # Main area of the code.
 def jobhunt(arg_dict):
     # Fetch jobs from website
@@ -90,18 +101,20 @@ def jobhunt(arg_dict):
 
     ## EXTRA CREDIT: Add your code to delete old entries
 
+
 # Setup portion of the program. Take arguments and set up the script
-# You should not need to edit anything here.
 def main():
     # Connect to SQL and get cursor
     conn = connect_to_sql()
     cursor = conn.cursor()
-    create_tables(cursor, "table")
+    create_tables(cursor, "jobs")
     # Load text file and store arguments into dictionary
-    arg_dict = load_config_file(sys.argv[1])
-    while(1):
-        jobhunt(arg_dict)
-        time.sleep(3600); # Sleep for 1h
+    arg_dict = load_config_file("config.cfg")
+    jobhunt(arg_dict)
+    #while(1):
+     #   jobhunt(arg_dict)
+     #   time.sleep(3600) # Sleep for 1h
+
 
 if __name__ == '__main__':
     main()
