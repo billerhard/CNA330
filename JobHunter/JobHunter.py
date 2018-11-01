7d770c30bfb9abd48b14821ee5e8178b40242dc6
Bill <wherhard@student.rtc.edu>
2018-11-01 16:07:24 -0700

    Cleaning up a bunch of stuff, making sure arguments work, etc.

--- JobHunter/JobHunter.py
+++ JobHunter/JobHunter.py
@@ -1,4 +1,5 @@
-# This script pulls from a job website and stores positions into a database. If there is a new posting it notifies the user.
+# This script pulls from a job website and stores positions into a database. If there is a new posting it notifies
+# the user.
 # CNA 330
 # Bill Erhard, wherhard@student.rtc.edu
 import mysql.connector
@@ -6,10 +7,8 @@ import sys
 import json
 import urllib.request
 import os
-import time
 
 
-# Connect to database
 def connect_to_sql():
     conn = mysql.connector.connect(user='root', password='',
                                    host='127.0.0.1',
@@ -17,60 +16,51 @@ def connect_to_sql():
     return conn
 
 
-# Create the table structure
-def create_tables(cursor, table):
-    cursor.execute(f"DROP TABLE {table}")
-    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} (id INT PRIMARY KEY, post_date TEXT, title TEXT, location "
-                   f"TEXT, full_part TEXT, description TEXT, apply_info TEXT, company TEXT, salary FLOAT, raw_message"
-                   f" TEXT  );")
+def create_tables(cursor, table, fields):
+    fields.insert(0, table)
+    fields = tuple(fields)
+    query = 'CREATE TABLE IF NOT EXISTS %s (%s INTEGER PRIMARY KEY AUTO_INCREMENT, %s TEXT, %s TEXT, %s TEXT, ' \
+            '%s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT);' % fields
+    cursor.execute(query)
     return
 
 
-# Query the database.
 def query_sql(cursor, query):
     cursor.execute(query)
     return cursor
 
 
-# Add a new job
 def add_new_job(cursor, jobdetails):
-    i = 0
     for job in jobdetails:
-        # YYYY-MM-DD HH:MM:SS
-        # github=Fri Oct 19 02:00:00 UTC 2018
+        if check_if_job_exists(cursor, job):
+            continue
         # https://stackoverflow.com/questions/7540803/escaping-strings-with-python-mysql-connector
-        data = (i, job['created_at'], job['title'], job['location'], job['type'], job['description'], job['how_to_apply'], job['company'], str(job))
-        # I had a fun time here for a sec because my wamp setup mysql to use latin swedish charset... wat?
-        # print(job)
-        sql = "INSERT INTO jobs (id, post_date, title, location, full_part, description, apply_info, company, raw_message) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        data = (job['id'], job['created_at'], job['title'], job['location'], job['type'], job['description'],
+                job['how_to_apply'], job['company'], "Bookoo Bucks", str(job))
+        sql = "INSERT INTO jobs (job_id, post_date, title, location, full_part, description, apply_info, company, " \
+              "salary, raw_message) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s); "
         cursor.execute(sql, data)
-        i += 1
 
 
 def check_if_job_exists(cursor, jobdetails):
-    ## Add your code here
-    query = "SELECT"
-    return query_sql(cursor, query)
+    query = 'SELECT %s FROM %s WHERE %s="%s";' % ("job_id", "jobs", "job_id", jobdetails['id'])
+    cursor.execute(query)
+    return cursor.fetchall()
 
 
 def delete_job(cursor, jobdetails):
-    ## Add your code here
     query = "UPDATE"
     return query_sql(cursor, query)
 
 
-# Grab new jobs from a website
 def fetch_new_jobs(arg_dict):
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/Sql.py
-    query = "https://jobs.github.com/positions.json"  # ?" + "&location=seattle" # + "&description=" + description # Add arguments here
+    query = "https://jobs.github.com/positions.json?location=%s&description=%s" % (arg_dict[1], arg_dict[3])
     jsonpage = 0
     try:
         contents = urllib.request.urlopen(query)
         response = contents.read()
         jsonpage = json.loads(response)
-
-
-
     except:
         print("whoops")
         pass
@@ -79,7 +69,7 @@ def fetch_new_jobs(arg_dict):
 
 # Load a text-based configuration file
 def load_config_file(filename):
-    argument_dictionary = 0
+    argument_dictionary = ""
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/FileIO.py
     rel_path = os.path.abspath(os.path.dirname(__file__))
     file_contents = 0
@@ -92,48 +82,27 @@ def load_config_file(filename):
         file.write("")
         file.close()
 
-    # for row in file_contents:
-    #     argument_dictionary += row
-    ## Add in information for argument dictionary
+    for row in file_contents:
+        argument_dictionary += row
     return argument_dictionary
 
 
 # Main area of the code.
 def jobhunt(arg_dict):
-    # Fetch jobs from website
+
     jobpage = fetch_new_jobs(arg_dict)
-    # print (jobpage)
     return jobpage
-    ## Add your code here to parse the job page
-
-    ## Add in your code here to check if the job already exists in the DB
-
-    ## Add in your code here to notify the user of a new posting
-
-    ## EXTRA CREDIT: Add your code to delete old entries
 
 
-# Setup portion of the program. Take arguments and set up the script
 def main():
-    # Connect to SQL and get cursor
-    fields = ["id", "post_date", "title", "location", "full_part", "description", "apply_info", "company", "salary",
-              "raw_message"]
+    fields = ["id", "job_id", "post_date", "title", "location", "full_part", "description", "apply_info", "company",
+              "salary", "raw_message"]
     conn = connect_to_sql()
     cursor = conn.cursor()
-    arg_dict = load_config_file(sys.argv[1])
-
-    create_tables(cursor, "jobs")
-    # Load text file and store arguments into dictionary
-
-    # config 1 tablename 2 location 3 0 4 description
-    j = jobhunt(arg_dict)
-
-    add_new_job(cursor, j)
-
-
-# while(1):
-#   jobhunt(arg_dict)
-#   time.sleep(3600) # Sleep for 1h
+    arg_dict = load_config_file(sys.argv[1]).split('\n')
+    create_tables(cursor, arg_dict[0], fields)
+    list_of_jobs = jobhunt(arg_dict)
+    add_new_job(cursor, list_of_jobs)
 
 
 if __name__ == '__main__':
