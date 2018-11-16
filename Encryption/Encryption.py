0f4afdb1f20d33d3325ba19acd538517e9ba3745
Bill <wherhard@student.rtc.edu>
2018-11-15 20:56:29 -0800

    hashing some things.

--- Encryption/Encryption.py
+++ Encryption/Encryption.py
@@ -8,14 +8,18 @@ def main():
     conn = mysql.connector.connect(user='root', password='',
                                    host='127.0.0.1', database='cna330')
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS encrypted (id INTEGER PRIMARY 
-    KEY AUTO_INCREMENT, username TEXT, password TEXT, credit_card TEXT, 
-    ssn TEXT);''')
-
+    # cursor.execute('''CREATE TABLE IF NOT EXISTS encrypted (id INTEGER PRIMARY
+    # KEY AUTO_INCREMENT, username TEXT, password TEXT, credit_card TEXT,
+    # ssn TEXT);''')
+    #
     cursor.execute('''INSERT INTO encrypted(username, password, credit_card, ssn
-    ) VALUES ("zrubin", "correcthorsebatterystaple", "4001 4246 1234 5678", 
+    ) VALUES ("zrubin", SHA2("correcthorsebatterystaple", 256), "4001 4246 1234 
+    5678",
     "000-00-0000");''')
 
+    # cursor.execute('''SELECT SHA2('', 256);''')
+    # result = cursor.fetchall()
+    # print(result)
 
     conn.commit()
     conn.close
