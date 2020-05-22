b743af4fc4f9c03cdb51af0a15c79f4078a6d4ee
bill <wherhard@student.rtc.edu>
2020-05-22 15:44:58 -0700

    Updates

--- JobHunter/JobHunter.py
+++ JobHunter/JobHunter.py
@@ -2,7 +2,8 @@
 # the user.
 # CNA 330
 # Bill Erhard, wherhard@student.rtc.edu
-import mysql.connector
+import sqlite3
+
 import sys
 import json
 import urllib.request
@@ -10,58 +11,39 @@ import os
 
 
 def connect_to_sql():
-    conn = mysql.connector.connect(user='root', password='',
-                                   host='127.0.0.1',
-                                   database='cna330')
+    conn = sqlite3.connect("jobs.db")
     return conn
 
 
-def create_tables(cursor, table, fields):
-    fields.insert(0, table)
+def create_tables(cursor, fields):
     fields = tuple(fields)
-    query = '''CREATE TABLE IF NOT EXISTS %s (%s INTEGER PRIMARY KEY AUTO_INCREMENT, %s TEXT, %s TEXT, %s TEXT, ''' \
-            '''%s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT);''' % fields
+    query = '''CREATE TABLE IF NOT EXISTS jobs (%s TEXT, %s TEXT, %s TEXT, ''' \
+            '''%s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT, %s TEXT);''' % fields
 
-    return query_sql(cursor, query)
-
-
-def query_sql(cursor, query):
-    cursor.execute(query)
-    return cursor
+    return cursor.execute(query)
 
 
 def add_new_job(cursor, job_details):
     # https://stackoverflow.com/questions/7540803/escaping-strings-with-python-mysql-connector
 
-    data = (job_details['id'], job_details['created_at'], job_details['title'], job_details['location'], job_details['type'], job_details['description'],
-            job_details['how_to_apply'], job_details['company'], "Bookoo Bucks", str(job_details))
-    sql = "INSERT INTO jobs (job_id, post_date, title, location, full_part, description, apply_info, company, " \
-          "salary, raw_message) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s); "
-
+    data = (job_details['id'], job_details['type'], job_details['url'], job_details['created_at'],
+            job_details['company'], job_details['company_url'], job_details['location'], job_details['title'],
+            job_details['description'], job_details['how_to_apply'], job_details['company_logo'])
+    sql = "INSERT INTO jobs VALUES (?,?,?,?,?,?,?,?,?,?,?);"
 
     cursor.execute(sql, data)
 
 
 def check_if_job_exists(cursor, jobdetails):
-    query = '''SELECT %s FROM %s WHERE %s="%s";''' % ("job_id", "jobs", "job_id", jobdetails['id'])
+    query = '''SELECT job_id FROM jobs WHERE job_id="%s";''' % (jobdetails['id'])
     cursor.execute(query)
     return cursor.fetchall()
 
 
-def get_job_id(cursor, jobdetails):
-
-    pass
-
-
-def delete_job(cursor, jobdetails):
-    job_id = get_job_id(cursor, jobdetails)
-    query = '''DELETE FROM %s WHERE %s=%s''' % ("jobs", "id", job_id)
-    return query_sql(cursor, query)
-
-
-def fetch_new_jobs(arg_dict):
+def fetch_new_jobs(arg_dict, page):
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/Sql.py
-    query = "https://jobs.github.com/positions.json?location=%s&description=%s" % (arg_dict[1], arg_dict[3])
+    query = "https://jobs.github.com/positions.json?location=%s&description=%s&page=%s" % \
+            (arg_dict[1], arg_dict[3], page)
     jsonpage = 0
     try:
         contents = urllib.request.urlopen(query)
@@ -70,13 +52,13 @@ def fetch_new_jobs(arg_dict):
     except:
         print("whoops")
         pass
+    #print(jsonpage)
     return jsonpage
 
 
 def load_config_file(filename):
     argument_dictionary = ""
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/FileIO.py
-    rel_path = os.path.abspath(os.path.dirname(__file__))
     file_contents = 0
     try:
         file = open(filename, "r")
@@ -92,23 +74,41 @@ def load_config_file(filename):
     return argument_dictionary
 
 
-def jobhunt(cursor, arg_dict):
-    jobpage = fetch_new_jobs(arg_dict)
+def add_jobs_from_page(cursor, jobpage):
     for job in jobpage:
         if check_if_job_exists(cursor, job):
             continue
         add_new_job(cursor, job)
-    return jobpage
+
+
+def jobhunt(cursor, arg_dict):
+    page = 0
+    jobpage = fetch_new_jobs(arg_dict, page)
+    while True:
+        if jobpage:
+            add_jobs_from_page(cursor, jobpage)
+            page += 1
+            jobpage = fetch_new_jobs(arg_dict, page)
+        else:
+            break
+
+
+def display_jobs(cursor):
+    for job in cursor.execute('SELECT * FROM jobs;'):
+        print(job)
 
 
 def main():
-    fields = ["id", "job_id", "post_date", "title", "location", "full_part", "description", "apply_info", "company",
-              "salary", "raw_message"]
+    fields = ["job_id", "type", "url", "created_at", "company", "company_url", "location", "title", "description",
+              "how_to_apply", "company_logo"]
     conn = connect_to_sql()
     cursor = conn.cursor()
     arg_dict = load_config_file(sys.argv[1]).split('\n')
-    create_tables(cursor, arg_dict[0], fields)
+    create_tables(cursor, fields)
     jobhunt(cursor, arg_dict)
+    conn.commit()
+    display_jobs(cursor)
+    conn.close()
 
 
 if __name__ == '__main__':
